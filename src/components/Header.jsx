1ecfa291fe7bfb782edda563e0514d385327b920
Gheorghe <madangheorghe.30@gmail.com>
2025-03-18 18:57:31 +0100

    feat: add mobile menu toggle to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,81 @@
 import React from "react";
+
+// Importo lo style del header
 import "../style/header.css";
+
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FaMoon, FaFlag, FaGlobe } from "react-icons/fa";
+import { FaMoon, FaFlag, FaGlobe, FaBars } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
+// Importo useState
+import { useState } from "react";
+
+
+
+
+
 const Header = () => {
+
+  // Stato per controllare la visibilità del menu
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
+
   return (
-    <header className="custom-header">
-      <div className="container d-flex align-items-center justify-content-between">
-        {/* Logo */}
-        <div className="logo">
-          <img src="/logo_desktop.png" alt="Logo" />
-        </div>
+    <>
+      <header className="custom-header">
+        <div className="container d-flex align-items-center justify-content-between" id="container-nav">
+          {/* Logo */}
+          <div className="logo">
+            <img src="/logo_desktop.png" alt="Logo" />
+          </div>
 
-        {/* Navbar */}
-        <nav className="navbar">
-          <ul className="nav fw-bolder">
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link">Home</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link">Viaggi</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/new_trip" className="nav-link">Aggiungi un nuovo viaggio</NavLink>
-            </li>
-          </ul>
-        </nav>
+          {/* Navbar */}
+          <nav className="navbar">
+            <ul className="nav fw-bolder d-flex  flex-nowrap">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">Viaggi</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/new_trip" className="nav-link text-center">Aggiungi un nuovo viaggio</NavLink>
+              </li>
+            </ul>
+          </nav>
 
-        {/* Icone e Pulsante */}
-        <div className="header-icons">
-          <FaMoon className="icon" />
-          <FaFlag className="icon" />
-          <FaGlobe className="icon" />
-          <button className="btn btn-primary">Accedi</button>
+          {/* Navbar mobile */}
+
+          <nav className={`d-none  ${!menuOpen ? "open" : "menu"}`} id="menu-mobile">
+            <ul className="nav fw-bolder d-flex  flex-nowrap">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">Viaggi</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/new_trip" className="nav-link text-center">Aggiungi un nuovo viaggio</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+
+          {/* Icone e Pulsante */}
+          <div className="header-icons">
+            <FaMoon className="icon" />
+            <FaFlag className="icon" />
+            <FaGlobe className="icon" />
+            <button className="btn btn-primary">Accedi</button>
+            <FaBars className="icon menu" onClick={() => setMenuOpen(!menuOpen)} />
+
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+    </>
   );
 };
 
