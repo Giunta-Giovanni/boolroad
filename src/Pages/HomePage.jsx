13f2bb6fb8930d2abcacf46cc4895a43b6642124
Gheorghe <madangheorghe.30@gmail.com>
2025-03-18 11:47:23 +0100

    [UPDATE] enhance homepage layout and styling with search functionality

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,17 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../style/HomePageStyle.css';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function HomePage() {
     return (
         <>
-            <h1>questa è la home</h1>
+            {/* <h1>questa è la home</h1> */}
             <section className='jumbotron'>
                 <div>
                     <form action="#">
-                        <input type="text" />
+                        <input type="text" placeholder='Cerca un viaggio!' />
+                        <button>{<FontAwesomeIcon icon={faSearch} />}</button>
                     </form>
                 </div>
             </section>
-
         </>
     )
 }
