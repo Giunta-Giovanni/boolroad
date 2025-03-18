253d475c1601017789ea7b9e3430a4aab283b06b
Federico Spadaro <federicospadaro2@gmail.com>
2025-03-18 11:14:26 +0100

    [ADD] header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,42 @@
-export default function Header() {
-    return (
-        <>
-        </>
-    )
-}
+import React from "react";
+import "../style/header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaMoon, FaFlag, FaGlobe } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="custom-header">
+      <div className="container d-flex align-items-center justify-content-between">
+        {/* Logo */}
+        <div className="logo">
+          <img src="/logo.png" alt="Logo" />
+        </div>
+
+        {/* Navbar */}
+        <nav className="navbar">
+          <ul className="nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Servizi</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contatti</a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Icone e Pulsante */}
+        <div className="header-icons">
+          <FaMoon className="icon" />
+          <FaFlag className="icon" />
+          <FaGlobe className="icon" />
+          <button className="btn btn-primary">Accedi</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
