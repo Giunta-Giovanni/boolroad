db99fd9856a53233ef8f7a91ef0f9328d3ff7fc1
Gheorghe <madangheorghe.30@gmail.com>
2025-03-18 15:55:28 +0100

    style: update TripCard layout and enhance image styling

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -2,8 +2,8 @@ export default function TripCard({ tripProp }) {
     // destrutturazione delle props
     const { id, nome, dataPartenza, dataRitorno, image, abstract } = tripProp;
     return (
-        <div className="card col-12 col-sm-12 col-md-4 col-lg-3" style={{ width: "18rem" }}>
-            <img src={image} className="card-img-top" alt="..." />
+        <div className="col-12 col-sm-12 col-md-4 col-lg-3" style={{ width: "18rem" }}>
+            <img src={image} className="card-img-top rounded-4" alt={nome} style={{ width: "100%", height: "250px" }} />
             <div className="card-body">
                 <h5 className="card-title">{nome}</h5>
                 <span>Data partenza: {dataPartenza}</span><br />
