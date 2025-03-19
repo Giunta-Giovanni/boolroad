e0009e4225b79f6d8ab56cd89b8411fe51bebdf8
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-19 01:35:27 +0100

    [fix] card in homepage

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -11,24 +11,26 @@ export default function TripCard({ tripProp }) {
     const { id, nome, dataPartenza, dataRitorno, image } = tripProp;
     return (
 
-        <Link to={`trip_page/${id}`} className="text-decoration-none col-sm-12 col-md-6 col-lg-3">
-            <div className="card">
+
+        <div className="card  col-sm-12 col-md-6 col-lg-3">
+            <Link to={`trip_page/${id}`} className="text-decoration-none">
                 <div className="card-image">
                     <img src={image} className="card-img-top rounded-4" alt={`Image for trip to ${nome}`} />
                 </div>
                 <div className="card-body p-3">
                     <h5 className="card-title fw-bolder mt-2 text-center">{nome}</h5>
                     <div className="d-flex justify-content-between mt-3">
-                        <span className="d-flex align-items-center">
+                        <span className="date ">
                             <FontAwesomeIcon icon={faPlaneDeparture} className="text-primary me-2" /> {dataPartenza}
                         </span>
-                        <span className="d-flex align-items-center">
+                        <span className=" date ">
                             <FontAwesomeIcon icon={faPlaneArrival} className="text-success me-2" /> {dataRitorno}
                         </span>
                     </div>
                 </div>
-            </div>
-        </Link>
+            </Link>
+
+        </div >
 
 
     )
